1af83fd390fbe2b69673ca08e1f7e660dd3ac55e
Sami Hindi <78088687+FujiwaraChoki@users.noreply.github.com>
2023-04-28 20:18:43 +0200

    Looping through everything

--- src/main.py
+++ src/main.py
@@ -105,22 +105,23 @@ def say(text):
 def main():
     r = sr.Recognizer()
 
-    with sr.Microphone() as source:
-        print("Speak now:")
-        audio = r.listen(source)
+    while True:
+        with sr.Microphone() as source:
+            print("Speak now:")
+            audio = r.listen(source)
 
-    try:
-        response_text = r.recognize_google(audio)
+        try:
+            response_text = r.recognize_google(audio)
 
-        gpt_response = generate_answer(response_text)
+            gpt_response = generate_answer(response_text)
 
-        say(gpt_response)
+            say(gpt_response)
 
-    except sr.UnknownValueError:
-        say("Sorry, I could not understand what you said.")
-    except sr.RequestError as e:
-        say(
-            "Could not request results from Google Speech Recognition service; {0}".format(e))
+        except sr.UnknownValueError:
+            say("Sorry, I could not understand what you said.")
+        except sr.RequestError as e:
+            say(
+                "Could not request results from Google Speech Recognition service; {0}".format(e))
 
 
 if __name__ == "__main__":
